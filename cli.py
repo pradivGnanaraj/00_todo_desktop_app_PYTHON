1d2b6726f7dc2a4a9d1c509850d8a8b05c318674
pradivgnanaraj <tpradivgnanaraj@gmail.com>
2023-07-19 19:41:21 +0530

    Tested with python3.10

---
+++ cli.py
@@ -0,0 +1,55 @@
+from modules import functions
+import time
+
+now = time.strftime("%b %d, %Y :::Time::  %H:%M:%S ")
+print("Current Time :: ")
+
+while True:
+    user_action = input("Type add, show, edit, complete or exit: ")
+    user_action = user_action.strip()
+
+    if user_action.startswith("add"):
+        todo = user_action[4:]
+        todos = functions.get_todos()
+        todos.append(todo + '\n')
+        # Context Manager
+        functions.write_todos(todos)
+
+    elif user_action.startswith("show"):
+        todos = functions.get_todos()
+        for index, items in enumerate(todos):
+            items = items.strip('\n') # removing the additional '\n'
+            row = f"{index + 1}-{items}"
+            print(row)
+
+    elif user_action.startswith("edit"):
+        try:
+            number = int(user_action[5:])
+            number = number - 1
+            todos = functions.get_todos()
+            new_todo = input("Enter the new todo: ")
+            todos[number] = new_todo + '\n'
+            functions.write_todos(todos)
+        except ValueError:
+            print("Your command is not valid")
+            continue
+
+    elif user_action.startswith("complete"):
+        try:
+            number = int(user_action[9:])
+            todos = functions.get_todos()
+            index = number - 1
+            todo_to_remove = todos[index].strip('\n')
+            todos.pop(index)
+            functions.write_todos(todos)
+            message = f"Todo {todo_to_remove} was removed cd from the list"
+        except IndexError:
+            print("There is no item with that number")
+            continue
+
+    elif 'exit' in user_action:
+        break
+
+    else:
+        print("***** Invalid Input ******")
+exit("Good Bye!")
---
+++ gui.py
@@ -0,0 +1,80 @@
+import time
+import os
+from modules import functions
+import PySimpleGUI as PySG
+
+if not os.path.exists("todos.txt"):
+    with open("todos.txt", 'w') as file:
+        pass
+
+PySG.theme("Black")
+
+clock = PySG.Text('', key='clock')
+label = PySG.Text("Type in a to-do")
+input_box = PySG.InputText(tooltip="Enter todo", key="todo")
+add_button = PySG.Button("Add")
+list_box = PySG.Listbox(values=functions.get_todos(),
+                        key='todos',
+                        enable_events=True,
+                        size=[45, 10]
+                        )
+edit_button = PySG.Button("Edit")
+complete_button = PySG.Button("Complete")
+exit_button = PySG.Button("Exit")
+
+window = PySG.Window("My To-Do App",
+                     layout=[
+                         [clock],
+                         [label],
+                         [input_box, add_button],
+                         [list_box, edit_button, complete_button],
+                         [exit_button]
+                     ],
+                     font=('Helvetica', 20)
+                     )
+
+while True:
+    event, values = window.read(timeout=200)
+    window['clock'].update(value=time.strftime("%b %d, %Y :::Time::  %H:%M:%S "))
+
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['todo'] + '\n'
+            todos.append(new_todo)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+
+        case "Edit":
+            try:
+                todo_to_edit = values['todos'][0]
+                new_todo = values['todo']
+                todos = functions.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
+            except IndexError:
+                PySG.popup("Please select an item first", font=("Helvetica", 20))
+
+        case "Complete":
+            try:
+                todo_to_complete = values['todos'][0]
+                todos = functions.get_todos()
+                todos.remove(todo_to_complete)
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
+                window['todo'].update(value='')
+            except IndexError:
+                PySG.popup("Please select an item first", font=("Helvetica", 20))
+
+        case "Exit":
+            break
+
+        case "todos":
+            window['todo'].update(value=values['todos'][0])
+
+        case PySG.WIN_CLOSED:
+            break
+
+window.close()
